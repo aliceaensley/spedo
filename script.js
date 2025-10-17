21a9dfdf22a00edd1fc7b447856b868336bb82ef
aliceaensley <ainsleyalice6@gmail.com>
2025-10-17 09:48:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function setEngine(state) {
 function setSpeed(speed) {
     switch(speedMode)
     {
-        case 1: speed = elements.speed.innerText = `${Math.round(speed * 2.236936)} MPH`; break;
-        case 2: speed = elements.speed.innerText = `${Math.round(speed * 1.943844)} Knots`; break;
-        default: speed = elements.speed.innerText = `${Math.round(speed * 3.6)} KMH`;
+        case 1: elements.speed.innerText = `${Math.round(speed * 2.236936)} MPH`; break;
+        case 2: elements.speed.innerText = `${Math.round(speed * 1.943844)} Knots`; break;
+        default: elements.speed.innerText = `${Math.round(speed * 3.6)} KMH`;
     }
 }
 
@@ -33,39 +33,6 @@ function setGear(gear) {
     elements.gear.innerText = String(gear);
 }
 
-function setHeadlights(state) {
-    switch(state)
-    {
-        case 1: elements.headlights.innerText = 'On'; break;
-        case 2: elements.headlights.innerText = 'High Beam'; break;
-        default: elements.headlights.innerText = 'Off';
-    }
-}
-
-function setLeftIndicator(state) {
-    indicators = (indicators & 0b10) | (state ? 0b01 : 0b00);
-    elements.indicators.innerText = `${indicators & 0b01 ? 'On' : 'Off'} / ${indicators & 0b10 ? 'On' : 'Off'}`;
-}
-
-function setRightIndicator(state) {
-    indicators = (indicators & 0b01) | (state ? 0b10 : 0b00);
-    elements.indicators.innerText = `${indicators & 0b01 ? 'On' : 'Off'} / ${indicators & 0b10 ? 'On' : 'Off'}`;
-}
-
-function setSeatbelts(state) {
-    elements.seatbelts.innerText = onOrOff(state);
-}
-
-function setSpeedMode(mode) {
-    speedMode = mode;
-    switch(mode)
-    {
-        case 1: elements.speedMode.innerText = 'MPH'; break;
-        case 2: elements.speedMode.innerText = 'Knots'; break;
-        default: elements.speedMode.innerText = 'KMH';
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     elements = {
         engine: document.getElementById('engine'),
@@ -74,9 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
         fuel: document.getElementById('fuel'),
         health: document.getElementById('health'),
         gear: document.getElementById('gear'),
-        headlights: document.getElementById('headlights'),
-        indicators: document.getElementById('indicators'),
-        seatbelts: document.getElementById('seatbelts'),
-        speedMode: document.getElementById('speed-mode'),
     };
 });
