10ad366633eec6223e5a662514dfb3395bfd5cd1
aliceaensley <ainsleyalice6@gmail.com>
2025-10-17 09:49:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
 let elements = {};
 let speedMode = 1;
-let indicators = 0;
 
 const onOrOff = state => state ? 'On' : 'Off';
 
-function setEngine(state) {
-    elements.engine.innerText = onOrOff(state);
-}
-
+function setEngine(state) { elements.engine.innerText = onOrOff(state); }
 function setSpeed(speed) {
-    switch(speedMode)
-    {
-        case 1: elements.speed.innerText = `${Math.round(speed * 2.236936)} MPH`; break;
-        case 2: elements.speed.innerText = `${Math.round(speed * 1.943844)} Knots`; break;
-        default: elements.speed.innerText = `${Math.round(speed * 3.6)} KMH`;
+    let val;
+    switch(speedMode) {
+        case 1: val = Math.round(speed * 2.236936) + ' MPH'; break;
+        case 2: val = Math.round(speed * 1.943844) + ' Knots'; break;
+        default: val = Math.round(speed * 3.6) + ' KMH';
     }
+    elements.speed.innerText = val;
+    elements.speed.classList.add('dd-neon');
 }
 
 function setRPM(rpm) {
-    elements.rpm.innerText = `${rpm.toFixed(4)} RPM`;
+    elements.rpm.innerText = `${rpm.toFixed(0)} RPM`;
+    elements.rpm.classList.add('dd-neon');
 }
 
-function setFuel(fuel) {
-    elements.fuel.innerText = `${(fuel * 100).toFixed(1)}%`;
-}
-
-function setHealth(health) {
-    elements.health.innerText = `${(health * 100).toFixed(1)}%`;
-}
-
-function setGear(gear) {
-    elements.gear.innerText = String(gear);
-}
+function setFuel(fuel) { elements.fuel.innerText = `${(fuel*100).toFixed(0)}%`; }
+function setHealth(health) { elements.health.innerText = `${(health*100).toFixed(0)}%`; }
+function setGear(gear) { elements.gear.innerText = String(gear); }
 
 document.addEventListener('DOMContentLoaded', () => {
     elements = {
