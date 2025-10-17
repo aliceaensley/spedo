8d8f1b2e653b8cc4fa496224aad7ec6a4e886b1d
aliceaensley <ainsleyalice6@gmail.com>
2025-10-17 13:04:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let indicators = 0;
 function onOrOff(state) { return state ? 'On' : 'Off'; }
 
 function setEngine(state) { elements.engine.innerText = onOrOff(state); }
+
 function setSpeed(speed) {
     let val;
     switch(speedMode) {
@@ -15,20 +16,30 @@ function setSpeed(speed) {
     elements.speed.innerText = val;
     elements.speedDisplay.textContent = val;
 }
+
 function setRPM(rpm) {
     let val = Math.round(rpm * 8000) + ' RPM';
     elements.rpm.innerText = val;
     elements.rpmDisplay.textContent = val;
 }
+
 function setFuel(fuel) { elements.fuel.innerText = (fuel*100).toFixed(1)+'%'; }
 function setHealth(health) { elements.health.innerText = (health*100).toFixed(1)+'%'; }
 function setGear(gear) { elements.gear.innerText = gear===0?'N':gear; }
-function setHeadlights(state) {
-    elements.headlights.innerText = state===1?'On':state===2?'High Beam':'Off';
+function setHeadlights(state) { elements.headlights.innerText = state===1?'On':state===2?'High Beam':'Off'; }
+
+function setLeftIndicator(state){
+    indicators=(indicators&0b10)|(state?0b01:0);
+    elements.indicators.innerText = `${indicators&0b01?'On':'Off'} / ${indicators&0b10?'On':'Off'}`;
 }
-function setLeftIndicator(state){ indicators=(indicators&0b10)|(state?0b01:0); elements.indicators.innerText = `${indicators&0b01?'On':'Off'} / ${indicators&0b10?'On':'Off'}`;}
-function setRightIndicator(state){ indicators=(indicators&0b01)|(state?0b10:0); elements.indicators.innerText = `${indicators&0b01?'On':'Off'} / ${indicators&0b10?'On':'Off'}`;}
+
+function setRightIndicator(state){
+    indicators=(indicators&0b01)|(state?0b10:0);
+    elements.indicators.innerText = `${indicators&0b01?'On':'Off'} / ${indicators&0b10?'On':'Off'}`;
+}
+
 function setSeatbelts(state){ elements.seatbelts.innerText = onOrOff(state); }
+
 function setSpeedMode(mode){
     speedMode=mode;
     elements.speedMode.innerText = mode===1?'MPH':mode===2?'Knots':'KMH';
